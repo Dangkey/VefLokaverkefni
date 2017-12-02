f9b94cdcd6a88a2a1b791334db2760c9b2ed1789
Dangkey <hillisnillig@gmail.com>
2017-12-02 00:39:14 +0000

    Lagaði physics og breytti smá útliti

--- js/main.js
+++ js/main.js
@@ -1,37 +1,60 @@
+			//Kallar í scriptur fyrir physics
 			Physijs.scripts.worker = 'js/physijs_worker.js';
 			Physijs.scripts.ammo = 'ammo.js';
+
+			//Variables
 			var container, camera, scene, renderer, geometry, group;
+			var loader = new THREE.TextureLoader();
 
-			var mouseX = 0,
-			    mouseY = 0;
 
+			//breytur sem ná í hálfa stærðina á browsernum sem verður notað í function seinna
 			var windowHalfX = window.innerWidth / 2;
 			var windowHalfY = window.innerHeight / 2;
 
-			document.addEventListener('mousemove', onDocumentMouseMove, false);
 
+
+			//Init býr til sceneið og restina og animate kallar á rendererinn þanni að það keyrist
 			init();
 			animate();
 
-
+			//Function sem býr til Sceneið og setur hluti inní það
 			function init() {
+				//býr til div sem sceneið er sett í
 			    container = document.createElement('div');
 			    document.body.appendChild(container);
 
-			    camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 1, 10000);
-			    camera.position.z = 600;
-
-			    scene = new Physijs.Scene();
+				//Býr til scene sem höndlar physics og breyti þar þyngdaraflinu
+				scene = new Physijs.Scene();
 			    scene.setGravity(new THREE.Vector3(0, -100, 0));
-			    scene.background = new THREE.Color(0xffffff);
-			    var texture = new THREE.TextureLoader().load('textures/crate.gif');
-				var ground_texture = new THREE.TextureLoader().load('textures/ground.jpg');
-			    var geometry = new THREE.BoxGeometry(10, 10, 10);
-			    var material = new THREE.MeshBasicMaterial({
-			        map: texture
-			    });
 
-			    // Materials
+				//Býr til aðal myndavélina og ræð hvar hún er staðsett
+			    camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 10000);
+			    camera.position.z = 800;
+				camera.position.y = 500;
+
+				//Loadar inn textures sem við notum
+			    var sky = loader.load('textures/sky.jpg');
+			    var ground_texture = loader.load('textures/ground.jpg');
+
+				//bý til background mesh sem við setjum svo texture á seinna sem mun vera bakgrunnurinn
+			    var bakgrunnur = new THREE.Mesh(
+			        new THREE.PlaneGeometry(2, 2, 0),
+			        new THREE.MeshBasicMaterial({
+			            map: sky
+			        })
+			    );
+
+			    // Notað til þess að láta bakgrunnin ekki hugsa um z-bufferið
+			    bakgrunnur.material.depthTest = false;
+			    bakgrunnur.material.depthWrite = false;
+
+				//Býr til scene þar sem bakgrunnurinn er geymdur og myndavél fyrir það.
+			    bakgrunnurScene = new THREE.Scene();
+			    bakgrunnurCam = new THREE.Camera();
+			    bakgrunnurScene.add(bakgrunnurCam);
+			    bakgrunnurScene.add(bakgrunnur);
+
+			    //materialið fyrir pallinn
 			    ground_material = Physijs.createMaterial(
 			        new THREE.MeshBasicMaterial({
 			            map: ground_texture
@@ -39,94 +62,72 @@
 			        .8,
 			        .4
 			    );
+				//lætur texturið á pallinum ná alla leiðina í staðinn fyrir að vera bara part af honum
 			    ground_material.map.wrapS = ground_material.map.wrapT = THREE.RepeatWrapping;
 			    ground_material.map.repeat.set(10, 10);
 
-			    // Ground
+			    // býr til pallinn
 			    ground = new Physijs.BoxMesh(
-			        new THREE.CubeGeometry(300, 1, 300),
+			        new THREE.BoxGeometry(1000, 1, 1000),
 			        ground_material,
 			        0 // mass
 			    );
-			    ground.receiveShadow = true;
-				ground.position.y = -400
 			    scene.add(ground);
 
-			    var parent = new Physijs.BoxMesh(geometry, material);
+				//breytur sem við notum þegar við búum til Kassana, geometry er notað til að gera Kassana,
+				// material er notað til þess að setja texture á kassana.
+			    var geometry = new THREE.BoxGeometry(20, 20, 20);
+			    var material = new THREE.MeshNormalMaterial();
 
-			    for (var i = 0; i < 1000; i++) {
 
+				//for loopa sem býr til mesh úr geometry og material, og gefur hverju meshi random staðsetningu.
+			    for (var i = 0; i < 500; i++) {
 			        var mesh = new Physijs.BoxMesh(geometry, material);
 			        mesh.__dirtyPosition = true;
-			        mesh.position.x = Math.random() * 200 - 100;
-			        mesh.position.y = Math.random() * 200 - 100;
-			        mesh.position.z = Math.random() * 200 - 100;
 
-					scene.add(mesh);
+			        mesh.position.x = Math.floor(Math.random() * 40 - 20) * 20;
+			        mesh.position.y = Math.floor(Math.random() * 40) * 20 + 10;
+			        mesh.position.z = Math.floor(Math.random() * 40 - 20) * 20;
 
-			        mesh.matrixAutoUpdate = false;
-			        mesh.updateMatrix();
-			        parent.add(mesh);
-
+			        scene.add(mesh);
 			    }
 
-			    scene.add(parent);
-
+				//býr til renderer sem er notaður til þess að birta allt sem við búum til.
 			    renderer = new THREE.WebGLRenderer({
-			        antialias: true
+			        antialias: true,
+			        alpha: true
 			    });
 			    renderer.setPixelRatio(window.devicePixelRatio);
 			    renderer.setSize(window.innerWidth, window.innerHeight);
+
+				//Þetta setur upp OrbitControls sem er notað til að gera zoomað inn og skoðað allt betur
 			    controls = new THREE.OrbitControls(camera, renderer.domElement);
+				//setur rendererinn í Divið sem við bjuggum til áðan
 			    container.appendChild(renderer.domElement);
 
-
-			    //
-
+				//event listener sem lætur rendererinn minnkast ef browserinn er minnkaður
 			    window.addEventListener('resize', onWindowResize, false);
 
 			}
-
+			//functionið til að minnka rendererinn
 			function onWindowResize() {
-
 			    windowHalfX = window.innerWidth / 2;
 			    windowHalfY = window.innerHeight / 2;
-
 			    camera.aspect = window.innerWidth / window.innerHeight;
 			    camera.updateProjectionMatrix();
-
 			    renderer.setSize(window.innerWidth, window.innerHeight);
-
-			}
-			controls = new THREE.OrbitControls(camera, renderer.domElement);
-
-			function onDocumentMouseMove(event) {
-
-			    mouseX = (event.clientX - windowHalfX) * 10;
-			    mouseY = (event.clientY - windowHalfY) * 10;
-
 			}
 
-			//
-
+			//kallar í renderinn og sýnir animations
 			function animate() {
-
 			    requestAnimationFrame(animate);
-
 			    render();
-
 			}
-
+			//functionið sem setur allt í rendererinn og simulatar physics á sceneinu
 			function render() {
 			    scene.simulate();
-
-			    camera.lookAt(scene.position);
-
-			    parent.__dirtyRotation = true;
-
-
-
+			    renderer.autoClear = false;
+			    renderer.clear();
+			    renderer.render(bakgrunnurScene, bakgrunnurCam);
 			    renderer.render(scene, camera);
-
-
 			}
